b83341e905e50114d2990afe218ee0ae4f90d5cc
marta <ffanecaa@gmail.com>
2022-08-08 20:44:02 +0200

    m

---
+++ js/practicando/ajax/ajax.js
@@ -0,0 +1,15 @@
+
+/*
+
+asincrono => 
+
+protocolo http 
+
+wikip 
+protocolo dde trnasferencia de hipertexto
+
+http://developer.mozilla.org/es/docs/web/HTTP/status
+
+
+las peticiones se hacen por php: apache
+
