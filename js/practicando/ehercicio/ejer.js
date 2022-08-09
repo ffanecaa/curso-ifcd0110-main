d9431d4b0128fa4a25d9354aaa92dc99258f4e95
marta <ffanecaa@gmail.com>
2022-08-09 19:12:02 +0200

    m

--- js/practicando/ehercicio/ejer.js
+++ js/practicando/ehercicio/ejer.js
@@ -2,14 +2,16 @@
 
 //let numa = prompt('escribe 10 numeros ')
 //let lista = nua.push()
-
+const lista =[]
 
 for ( let i=0; i<=4;i++) {
-    const lista = prompt( 'ponun numero')
+   const lista = prompt( 'ponun numero')
     
     console.log(lista)
 } 
 
 
 
-console.log(lista.sort((a,b)=>b-a)); // ordena de mayor a menor
+
+//console.log(lista.sort((a,b)=>b-a));
+ // ordena de mayor a menor
---
+++ js/practicando/ejercicios/ejercicio.js
@@ -0,0 +1,70 @@
+/*
+solicita nombre ,edad, .. muestra mensaje*/
+/*
+let nombre = prompt ('escribe tu nombre')
+let edad = prompt('escribe tu edad')
+
+document.getElementById('ele').innerHTML=(`hola ${nombre}, tienes ${edad} años y le proximo tendras ${parseInt(edad)+1} años`)
+*/
+
+
+/*calculael area figuras geometricas  
+
+let figura = prompt('escribe el nombre de una de las siguientes figuras geometricas: cuadrado, reptangulo o circulo')
+let altura;
+let base;
+let radio;
+
+switch(figura){
+    case 'triangulo':
+         base = prompt('indica la base')
+         altura =prompt('indica la altura')
+      document.getElementById('ele').innerHTML= `el area del triangulo es ${base*altura/2}`
+
+break
+case 'cuadrado':
+         base = prompt('indica la base')
+         altura =prompt('indica la altura')
+      document.getElementById('ele').innerHTML=( `el area del triangulo es ${base*altura}`)
+break
+case 'circulo':
+         radio = prompt('indica el radio')
+        
+      document.getElementById('ele').innerHTML=( `el area del triangulo es ${radio*radio*3.14}`)
+break
+}*/
+
+/* par/impar solicita numero imprime todos 
+los numeros vcon mensajesi par o impar*/
+/*
+
+let numero = prompt('indica un numero')
+
+for(i=0;i<numero;i++){
+
+   if( i%2==0){
+    document.getElementById('ele').innerHTML+= ` el numero ${i} es par .`;
+
+} else {
+ document.getElementById('ele').innerHTML += ` el numero ${i} es impar. `;
+}
+
+}*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
