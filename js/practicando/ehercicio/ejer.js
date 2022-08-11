56943f7a456bee457bce823fac7602fab0bd8427
ffanecaa <ffanecaa@gmail.com>
2022-08-11 12:35:23 +0200

    m

--- js/practicando/ehercicio/ejer.js
+++ js/practicando/ehercicio/ejer.js
@@ -2,13 +2,31 @@
 
 //let numa = prompt('escribe 10 numeros ')
 //let lista = nua.push()
-
-const numeros= [];
+/*
+let numeros=[]
 for ( let i=0; i<=4;i++) {
- numeros.push[i]=prompt( 'pon un numero')
-   console 
+ numeros=prompt( 'pon un numero')
+ console.log(numeros)
 } 
 
+const num =() =>{
+ prompt('escribe un numero')}
 
 
+
+for ( let i=0; i<=4;i++) {
+  let num = "prompt('escrbe un numero')"
+  console.log(num)
+}*/
+
+
+
+for ( let i=0; i<=4;i++) {
+ numeros=prompt( 'pon un numero')
+ c
  
+ 
+
+
+}
+
