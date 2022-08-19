918a6e2c4436e596a4bb9f624147bafca9d9eddf
ffanecaa <ffanecaa@gmail.com>
2022-08-19 12:54:33 +0200

    m

--- php/php/php.php
+++ php/php/php.php
@@ -2,8 +2,7 @@
      header("Access-Control-Allow-Origin: *");
      header("Content-Type: application/json; charset=UTF-8");
      header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, FETCH, OPTIONS");
-     header("Access-Control-Allow-Headers: Content-Type, Access- 
-     Control-Allow-Headers, Authorization, X-Requested-With");
+     header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
      $data = json_decode(file_get_contents('php://input'), true);
 
      echo json_encode($data);
