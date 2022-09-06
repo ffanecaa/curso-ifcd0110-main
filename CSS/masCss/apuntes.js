0b78d30eda5440af7e13b753ca26a40a8f40b634
marta <ffanecaa@gmail.com>
2022-09-06 17:22:36 +0200

    m

---
+++ CSS/masCss/apuntes.js
@@ -0,0 +1,23 @@
+/*metodologia CSS
+
+Ben:
+
+bloque: menu logo galeria tarjeta ....
+   usar clases sin ligaral html   guion bajo
+        ej classe logo
+        ej:
+        class card div
+              card__title  h1
+              card__text   p
+              card__image   img
+
+        ej redes sociales:
+
+    elementos    class social-icons   ul
+              social-icons__item   li
+              social-icons__icon  img
+
+   modificadores guion medio
+      card  la original
+      card--blue ypor casacada modificamos           
+
