918d0423b01cc339ab99544b2713aca13f627658
ffanecaa <ffanecaa@gmail.com>
2022-09-21 12:25:18 +0200

    m

--- base_datos/valida.php
+++ base_datos/valida.php
@@ -8,7 +8,20 @@ $edad = $_POST['age'];
 $contrasinal = $_POST['contrasinal'];
 $recontrasinal = $_POST['recontrasinal'];
 
+$fecha = date('j-m-y');// envia fecha del dia 
+
                                 // punto antes y despues para q concatene  // hueco entre comillas generar espacio 
 echo "los datos recibidos son:" .$nombre." " .$apellido." <br>".$Email." ".$direccion." " .$telefono." ".$edad."<br> ".$contrasinal." " .$recontrasinal;
+echo "<br>".$fecha;
+// vamos generar un archivo de texto no para tabla
+$archivo ="datosformulario.txt";// variable nombre archivo
+
+$creofichero = fopen ($archivo , "a"); // creamos funcion  pare que abra la fila con el nombre del archivo y la a es que vaya engadindo al final
+fwrite($creofichero, $Nombre."-" .$apellido. "-" .$Email. "-" .$edad."-" .$telefono."-" .$direccion."-" .$contrasinal."-" .$fecha. "\n"); //escribe en el fichero
+
+fclose($file);
+
+
+
 
 ?>
